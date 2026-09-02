b4e7f03ad14d63901b4048dc6c1ee0e167805f86
sk-dev-097 <sriramkarthik0.97@gmail.com>
2026-09-02 18:13:18 +0530

    Exercise-1 Problem 1.1.1 Completed

---
+++ Exercises/Exercise-1/Operators, Control Statements and Arrays/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(a<<2);
+        System.out.println(a>>2);
+
+        scanner.close();
+    }
+}
